d801598cc04dc27d2b621e4ab34121fd9d64ec4c
MrGelpo <mr.gelpo@gmail.com>
2022-02-22 12:09:22 +0500

    Add project files.

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController
+    {
+        //private readonly ILogger<StudentController> _logger;
+
+        //public StudentController(ILogger<StudentController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        [HttpGet(Name = "GetStudent")]
+        public IEnumerable<Student> Get()
+        {
+            return Enumerable.Range(1, 1).Select(index => new Student
+            {
+                Name = "Boris",
+                Age = 20,
+                StudentNumber = 123456,
+                Direction = "Math"
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ WebApplication1/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int StudentNumber { get; set; }
+        public string Direction { get; set; }
+    }
+}
